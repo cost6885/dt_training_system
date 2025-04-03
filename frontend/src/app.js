7f51b346ef2f6a4c26d890983d3ea7aa5c559369
cost6885 <132336835+cost6885@users.noreply.github.com>
2025-04-03 11:18:18 +0900

    Create app.js

---
+++ frontend/src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dashboard from './Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>DT Training System Dashboard</h1>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
