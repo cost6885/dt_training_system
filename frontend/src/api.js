a76fae19b2d9a3bda82f74efcb628ea040c35db5
cost6885 <132336835+cost6885@users.noreply.github.com>
2025-04-03 15:59:57 +0900

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,18 @@
-const API_URL = 'http://localhost:5000';
-
+// api.js
 export const getTrainingData = async () => {
-  const response = await fetch(`${API_URL}/employee/1/trainings`);
-  return await response.json();
+  const response = await fetch('http://15.164.228.135:5000/api/trainings');  // API URL
+  const data = await response.json();
+
+  console.log(data); // 데이터 형식을 확인
+
+  return data;  // 이 데이터를 배열로 반환해야 합니다.
 };
 
 export const getTaskData = async () => {
-  const response = await fetch(`${API_URL}/employee/1/tasks`);
-  return await response.json();
+  const response = await fetch('http://15.164.228.135:5000/api/tasks');  // API URL
+  const data = await response.json();
+
+  console.log(data); // 데이터 형식을 확인
+
+  return data;  // 이 데이터를 배열로 반환해야 합니다.
 };
